1a4e45c1285b6aecec128bcc95789519f50c7831
Eduardo Libório <eduardosolenomorizliborio@gmail.com>
2026-01-28 19:47:33 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,3 +7,9 @@ class Config:
     # 🔐 Senhas operacionais
     SENHA_ATESTADO = os.getenv("SENHA_ATESTADO", "1234")
     SENHA_ABONO = os.getenv("SENHA_ABONO", "5678")
+
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
+    GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+    GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
