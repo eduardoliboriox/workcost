775a163a8b66be90aa5939be248bebef44980272
Eduardo Libório <eduardosolenomorizliborio@gmail.com>
2026-01-31 23:35:22 -0400

    Update employees_repository.py

--- app/repositories/employees_repository.py
+++ app/repositories/employees_repository.py
@@ -6,7 +6,6 @@ def get_employee_by_matricula(matricula: str):
     """
     Busca funcionário completo para auto-fill da solicitação
     """
-
     with get_db() as conn:
         with conn.cursor(row_factory=dict_row) as cur:
             cur.execute("""
@@ -21,7 +20,8 @@ def get_employee_by_matricula(matricula: str):
                         p.state
                     ) AS endereco
                 FROM employees e
-                LEFT JOIN user_profiles p ON p.user_id = e.user_id
+                LEFT JOIN users u ON u.employee_id = e.id
+                LEFT JOIN user_profiles p ON p.user_id = u.id
                 WHERE e.employee_code = %s
                 LIMIT 1
             """, (matricula,))
